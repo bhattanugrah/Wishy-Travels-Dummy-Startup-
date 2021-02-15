11d45fb129296144fb49e7b7a712b8cdc52abf37
bhattanugrah <bhattanugrah@gmail.com>
2021-02-16 01:12:47 +0530

    PHP File

---
+++ wishy.js
@@ -0,0 +1,15 @@
+document.getElementById('signUp').addEventListener('click', function(){
+
+    document.querySelector('.bg-modal').style.display = 'flex';
+});
+
+document.getElementById('logIn').addEventListener('click', function(){
+
+    document.querySelector('.bg-modal').style.display = 'flex';
+
+});
+
+document.querySelector('.close').addEventListener('click', function(){
+
+    document.querySelector('.bg-modal').style.display = 'none';
+});
